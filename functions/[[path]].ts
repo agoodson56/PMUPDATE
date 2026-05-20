06a42886be5d62ca18395809ab730f6fabf3ef32
agoodson56 <agoodson56@gmail.com>
2026-05-19 17:53:16 -0700

    Pass through /static/* requests to Pages asset handler

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,4 +1,13 @@
 import { handle } from "hono/cloudflare-pages";
 import app from "../src/index";
 
-export const onRequest = handle(app);
+const honoHandler = handle(app);
+
+export const onRequest: PagesFunction = async (context) => {
+    const url = new URL(context.request.url);
+    // Let Pages serve static assets (CSS/JS/images) directly.
+    if (url.pathname.startsWith("/static/") || url.pathname === "/favicon.ico") {
+        return context.next();
+    }
+    return honoHandler(context);
+};
